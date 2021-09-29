bfc80721d102e0081db1aaa63ebaf9514dc250a8
Hari <smgdark@gmail.com>
2021-09-29 12:19:37 +0530

    Vue Config formatted

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,28 @@
-const {dependencies} = require('./package.json');
-const {vRenderer} = require('./vuelectro.config');
 const WebpackObfuscator = require('webpack-obfuscator');
+const { dependencies } = require('./package.json');
+const { vRenderer } = require('./vuelectro.config');
 
 module.exports = {
-    outputDir: './app/renderer',
-    publicPath: '', // This is needed for the Electron renderer to load webpacked Vue source
-    productionSourceMap: true, // Set this to false when doing final builds
-    configureWebpack: {
-        devtool: 'source-map',
-        target: 'electron-renderer',
-        externals: {
-            ...Object.keys(dependencies || {}).filter(d => !vRenderer.bundleIn.includes(d)).reduce((moduleObj, module) => (moduleObj[module] = `require('${module}')`, moduleObj), {})
-        },
-        plugins: [
-            ...vRenderer.obfuscate ? [new WebpackObfuscator(vRenderer.obfuscatorConfig)] : []
-        ]
-    }
+  outputDir: './app/renderer',
+  publicPath: '', // This is needed for the Electron renderer to load webpacked Vue source
+  productionSourceMap: true, // Set this to false when doing final builds
+  configureWebpack: {
+    devtool: 'source-map',
+    target: 'electron-renderer',
+    externals: {
+      ...Object.keys(dependencies || {})
+        .filter((d) => !vRenderer.bundleIn.includes(d))
+        .reduce(
+          (moduleObj, module) => (
+            (moduleObj[module] = `require('${module}')`), moduleObj
+          ),
+          {},
+        ),
+    },
+    plugins: [
+      ...(vRenderer.obfuscate
+        ? [new WebpackObfuscator(vRenderer.obfuscatorConfig)]
+        : []),
+    ],
+  },
 };
