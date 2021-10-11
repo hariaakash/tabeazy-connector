ac75e9114705d38b6ed29b286fbc29eff5385677
hariaakash <smgdark@gmail.com>
2021-10-11 17:09:20 +0530

    run cron task only between 7-23hr

--- src/helpers/cron.js
+++ src/helpers/cron.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 
 const createCron = (min, cb) => {
-  const expr = `*/${min} * * * *`;
+  const expr = `*/${min} 7-23 * * *`;
   const task = cron.schedule(expr, cb);
 
   return task;
